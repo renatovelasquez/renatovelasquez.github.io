0163f9bd93a478914e8447ddd3bb250d1e115e72
RVelasquez <velasquez.ren@gmail.com>
2021-06-18 21:10:22 -0400

    generate pdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,16 +93,28 @@ function removeScale() {
 
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
+let areaCv = document.getElementById('area-cv')
 let resumeButton = document.getElementById('resume-button')
 // Html2pdf options
+let opt = {
+    margin: 0,
+    filename: 'renvl.dev.pdf',
+    image: {type: 'jpeg', quality: 0.98},
+    html2canvas: {scale: 4},
+    jsPDF: {unit: 'in', format: 'A4', orientation: 'portrait'}
+}
 
-// Function to call areaCv and Html2Pdf options 
+// Function to call areaCv and Html2Pdf options
+function generatedResume() {
+    html2pdf(areaCv, opt)
+}
 
 // When the button is clicked, it executes the three functions
 resumeButton.addEventListener('click', () => {
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
     scaleCv()
     // 2. The PDF is generated
-
+    generatedResume()
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale, 5000)
 })
