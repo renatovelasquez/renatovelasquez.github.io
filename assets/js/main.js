fca96f77501c321dff217d0cb409f46ee9ce11d9
RVelasquez <velasquez.ren@gmail.com>
2021-06-18 20:36:46 -0400

    scale-cv insert and remove functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,28 +80,29 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
 
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
+function scaleCv() {
+    document.body.classList.add('scale-cv')
+}
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
-
+function removeScale() {
+    document.body.classList.remove('scale-cv')
+}
 
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
-
-
+let resumeButton = document.getElementById('resume-button')
 // Html2pdf options
-
 
 // Function to call areaCv and Html2Pdf options 
 
-
 // When the button is clicked, it executes the three functions
+resumeButton.addEventListener('click', () => {
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+    scaleCv()
+    // 2. The PDF is generated
 
-// 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
-
-// 2. The PDF is generated
-
-
-// 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+})
